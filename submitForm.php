2889f8c89e9ab93958cb660fdc2cb53b1ce5e12f
davemuir <davidmuirdesign@gmail.com>
2013-04-29 13:51:32 -0400

    idk

--- submitForm.php
+++ submitForm.php
@@ -63,10 +63,10 @@ if(isset($_POST['email'])) {
      
      
     	// create email headers
-	$headers = 'From: '.$email_from."\r\n".
-		'Reply-To: '.$email_from."\r\n" .
+	$headers = 'From:' .$email_from. "\r\n".
+		'Reply-To:' .$email_from. "\r\n" .
 		'X-Mailer: PHP/' . phpversion();
-		mail($email_to, $email_subject, $email_message, $headers);  
+		mail('davidmuirdesign@gmail.com', $email_subject, $email_message, $headers);  
 ?>
  
 <!-- include your own success html here -->
